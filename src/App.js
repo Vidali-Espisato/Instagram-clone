f0a5f7f06523e4d2a376e0a4fdca382957da6742
Manish Roy <manish.11612939@lpu.in>
2020-12-19 23:15:30 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi all!</h1>
+      <div className="App__header">
+        <img className="App__headerImage" src="https://fontmeme.com/images/instagram-new-logo.png"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
